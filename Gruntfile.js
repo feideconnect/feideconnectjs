d7f8e682ff2bb0cbf412c1f942d7b545c6a3cb36
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-07-06 14:25:24 +0200

    Add grunt and npm config for doing versioning and release

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+	"use strict";
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		bower: grunt.file.readJSON('bower.json'),
+		bump: {
+			options: {
+				files: ['package.json', 'bower.json'],
+				updateConfigs: ['pkg', 'bower'],
+
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['package.json'],
+
+				createTag: true,
+				tagName: 'v%VERSION%',
+				tagMessage: 'Version %VERSION%',
+
+				push: true,
+				pushTo: 'origin',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+				globalReplace: false,
+				prereleaseName: false,
+				regExp: false
+			}
+		},
+	});
+
+
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-bump');
+
+	// Default task(s).
+	grunt.registerTask('default', []);
+
+};
